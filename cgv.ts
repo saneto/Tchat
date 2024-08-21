9887f5197e29d24e4917480781b5eff6eb93f179
saneto <aymanezaircha@gmail.com>
2024-08-21 10:55:46 +0200

    Update

---
+++ cgv.ts
@@ -0,0 +1,6 @@
+const MESSAGES: Record<keyof MyModel, string> = {
+  name: 'Name is required.',
+  age: 'Age must be a valid number.',
+  address: 'Address cannot be empty.',
+  skills: 'Skills list cannot be empty.'
+};
